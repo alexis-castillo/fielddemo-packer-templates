369dea87610f12a1d3fd442a22e92fc08a1d4b3c
Alexis Castillo <alexis.castillo@quest.com>
2022-05-02 17:30:25 -0500

    commit on master

--- scripts/win2016.ps1
+++ scripts/win2016.ps1
@@ -4,6 +4,11 @@
 # @source https://github.com/virtualhobbit
 $ErrorActionPreference = "Stop"
 
+#############
+# turn off PowerShell execution policy restrictions
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
+#############
+
 # Switch network connection to private mode
 # Required for WinRM firewall rules
 $profile = Get-NetConnectionProfile
@@ -14,53 +19,15 @@ winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 
+#############
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{CredSSP="true"}'
+winrm set winrm/config/client '@{TrustedHosts="*"}'
+# open port 5985 in the internal Windows firewall to allow WinRM communication
+netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+#############
+
+
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
-
-
-###############################################################################################
-
-Write-Output "Disabling WinRM over HTTP..."
-Disable-NetFirewallRule -Name "WINRM-HTTP-In-TCP"
-Disable-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC"
-Get-ChildItem WSMan:\Localhost\listener | Remove-Item -Recurse
-
-Write-Output "Configuring WinRM for HTTPS..."
-Set-Item -Path WSMan:\LocalHost\MaxTimeoutms -Value '1800000'
-Set-Item -Path WSMan:\LocalHost\Shell\MaxMemoryPerShellMB -Value '1024'
-Set-Item -Path WSMan:\LocalHost\Service\AllowUnencrypted -Value 'false'
-Set-Item -Path WSMan:\LocalHost\Service\Auth\Basic -Value 'true'
-Set-Item -Path WSMan:\LocalHost\Service\Auth\CredSSP -Value 'true'
-
-New-NetFirewallRule -Name "WINRM-HTTPS-In-TCP" `
-    -DisplayName "Windows Remote Management (HTTPS-In)" `
-    -Description "Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]" `
-    -Group "Windows Remote Management" `
-    -Program "System" `
-    -Protocol TCP `
-    -LocalPort "5986" `
-    -Action Allow `
-    -Profile Domain,Private
-
-# New-NetFirewallRule -Name "WINRM-HTTPS-In-TCP-PUBLIC" `
-#     -DisplayName "Windows Remote Management (HTTPS-In)" `
-#     -Description "Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]" `
-#     -Group "Windows Remote Management" `
-#     -Program "System" `
-#     -Protocol TCP `
-#     -LocalPort "5986" `
-#     -Action Allow `
-#     -Profile Public
-
-$Hostname = [System.Net.Dns]::GetHostByName((hostname)).HostName.ToUpper()
-$pfx = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $Hostname
-$certThumbprint = $pfx.Thumbprint
-$certSubjectName = $pfx.SubjectName.Name.TrimStart("CN = ").Trim()
-
-New-Item -Path WSMan:\LocalHost\Listener -Address * -Transport HTTPS -Hostname $certSubjectName -CertificateThumbPrint $certThumbprint -Port "5986" -force
-
-Write-Output "Restarting WinRM Service..."
-Stop-Service WinRM
-Set-Service WinRM -StartupType "Automatic"
-Start-Service WinRM
